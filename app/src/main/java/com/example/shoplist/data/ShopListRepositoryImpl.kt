82447d1f17f41f5592ec2de514b7c3ad39fc5298
Vadim Shvets <vadimshvets@gmail.com>
2022-04-11 02:00:23 +0300

    data_sloi_end

---
+++ app/src/main/java/com/example/shoplist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.example.shoplist.data
+
+import com.example.shoplist.domain.ShopItem
+import com.example.shoplist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0;
+
+    override fun insertShopItem(shopItem: ShopItem) {
+        if(shopItem.id == ShopItem.UNDEFINED_ID){
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        insertShopItem(shopItem)
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId } ?: throw RuntimeException("Элемент id $shopItemId not found")
+    }
+}
