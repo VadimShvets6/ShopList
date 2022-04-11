9d5296e9551a506c0d01823d19d8bba6758427f4
Vadim Shvets <vadimshvets@gmail.com>
2022-04-11 23:44:15 +0300

    data_sloi_end

---
+++ app/src/main/java/com/example/shoplist/presentation/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.shoplist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoplist.data.ShopListRepositoryImpl
+import com.example.shoplist.domain.*
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun editShopItem(shopItem: ShopItem){
+        val newItem = shopItem.copy(enable = !shopItem.enable)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
